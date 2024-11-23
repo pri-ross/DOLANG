8e4d11cd36b28108be2b9e57f6b3917b9d399265
pri-ross <priscila.rossi@estudiantes.utec.edu.uy>
2024-11-23 10:50:32 -0300

    Update esclavo.c

--- lab4/codigos/ParteA/esclavo.c
+++ lab4/codigos/ParteA/esclavo.c
@@ -1,3 +1,6 @@
+spi_esclavo___spi
+
+
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
@@ -5,7 +8,7 @@
 #define MISO 4
 #define SCLK 5
 #define LED PD7     // LED conectado a PD7
-#define SERVO PBD5   // Servo conectado a PD6
+#define MOTOR PD3   // Servo conectado a PD6
 #define BUZZER PD6  // Buzzer conectado a PD5
 
 void SPI_SlaveInit();
@@ -14,15 +17,10 @@ void generateTone(uint16_t frequency);
 void servoRotate(uint8_t angle);
 
 int main() {
-	DDRD |= (1 << LED) | (1 << SERVO) | (1 << BUZZER); // Configura pines como salidas
-	PORTD &= ~((1 << LED) | (1 << SERVO) | (1 << BUZZER)); // Inicializa apagados
+	DDRD |= (1 << LED) | (1 << MOTOR) | (1 << BUZZER); // Configura pines como salidas
+	PORTD &= ~((1 << LED) | (1 << MOTOR) | (1 << BUZZER)); // Inicializa apagados
 
 	SPI_SlaveInit();
-
-	// Configuración de PWM para Servo
-	TCCR1A |= (1 << COM1A1) | (1 << WGM11);       // Modo PWM rápido (canal A)
-	TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescaler 8
-	ICR1 = 19999; // TOP para frecuencia de 50 Hz (20 ms período)
 
 	while (1) {
 		uint8_t received = SPI_SlaveReceive();
@@ -30,8 +28,8 @@ int main() {
 			case 'F': PORTD |= (1 << LED); break;  // Encender LED
 			case 'f': PORTD &= ~(1 << LED); break; // Apagar LED
 
-			case 'M': servoRotate(90); break; // Mover servo a 90 grados
-			case 'm': servoRotate(0); break;  // Mover servo a 0 grados
+			case 'M':  PORTD |= (1 << MOTOR); break;  // Encender MOTOR
+			case 'm':  PORTD &= ~(1 << MOTOR); break; // Apagar MOTOR 
 
 			case 'R':
 			PORTD |= (1 << BUZZER);
@@ -74,11 +72,4 @@ void generateTone(uint16_t frequency) {
 			}
 		}
 	}
-}
-
-void servoRotate(uint8_t angle) {
-	// El cálculo del pulso se ajusta a un rango de 1 ms a 2 ms para el servo
-	// Los servos típicos operan entre 1 ms (0 grados) y 2 ms (180 grados) con un periodo de 20 ms
-	uint16_t pulseWidth = (angle * 1000 / 180) + 1000; // 1 ms a 2 ms
-	OCR1A = pulseWidth;  // Asigna el valor del pulso
 }
