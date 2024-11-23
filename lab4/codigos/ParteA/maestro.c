04d4c383a37ed5ea02d621d254cb540280458b03
pri-ross <priscila.rossi@estudiantes.utec.edu.uy>
2024-11-23 10:50:00 -0300

    Update maestro.c

--- lab4/codigos/ParteA/maestro.c
+++ lab4/codigos/ParteA/maestro.c
@@ -16,6 +16,22 @@
 #define BUTTON_MOTOR PB1 // Botón para el motor conectado a PB1
 #define BUTTON_BUZZER PD2 // Botón para el buzzer conectado a PD2
 
+
+#define DHT_PIN PC0
+
+uint8_t humidity_int = 0;
+uint8_t temperature_int = 0;
+
+void UART_init(uint16_t ubrr);
+void UART_sendChar(char c);
+void UART_sendString(const char *str);
+void UART_sendInteger(int num);
+void DHT_start(void);
+uint8_t DHT_response(void);
+uint8_t DHT_read(void);
+void int_to_str(int num, char *str);
+
+
 uint8_t led_state = 0;   // Estado del LED
 uint8_t motor_state = 0; // Estado del motor
 uint8_t buzzer_state = 0; // Estado del buzzer
@@ -23,6 +39,88 @@ uint8_t buzzer_state = 0; // Estado del buzzer
 void SPI_MasterInit();
 void SPI_MasterTransmit(char data, char slave);
 uint8_t SPI_MasterReceive();
+
+
+void int_to_str(int num, char *str) {
+	int i = 0;
+	int is_negative = 0;
+
+	if (num < 0) {
+		is_negative = 1;
+		num = -num;
+	}
+
+	do {
+		str[i++] = (num % 10) + '0';
+		num /= 10;
+	} while (num > 0);
+
+	if (is_negative) {
+		str[i++] = '-';
+	}
+
+	str[i] = '\0';
+
+	for (int j = 0; j < i / 2; j++) {
+		char temp = str[j];
+		str[j] = str[i - j - 1];
+		str[i - j - 1] = temp;
+	}
+}
+
+void DHT_start() {
+	DDRC |= (1 << DHT_PIN);
+	PORTC &= ~(1 << DHT_PIN);
+	_delay_ms(20);
+	PORTC |= (1 << DHT_PIN);
+	_delay_us(50);
+}
+
+uint8_t DHT_response() {
+	DDRC &= ~(1 << DHT_PIN);
+	_delay_us(40);
+
+	if (!(PINC & (1 << DHT_PIN))) {
+		_delay_us(80);
+		
+		if (PINC & (1 << DHT_PIN)) {
+			_delay_us(80);
+			return 1;
+		}
+	}
+	
+	return 0;
+}
+
+uint8_t DHT_read() {
+	uint8_t result = 0;
+	
+	for (int i = 0; i < 8; i++) {
+		while (!(PINC & (1 << DHT_PIN))); // Espera pulso alto
+		_delay_us(30);
+		
+		if (PINC & (1 << DHT_PIN))
+		result |= (1 << (7 - i));
+		
+		while (PINC & (1 << DHT_PIN)); // Espera pulso bajo
+	}
+	
+	return result;
+}
+
+void read_DHT_data() {
+	DHT_start();
+	if (DHT_response()) {
+		humidity_int = DHT_read();      // Parte entera de la humedad
+		DHT_read();                     // Parte decimal de la humedad (ignorada)
+		temperature_int = DHT_read();   // Parte entera de la temperatura
+		DHT_read();                     // Parte decimal de la temperatura (ignorada)
+		DHT_read();                     // Checksum (ignoramos en este caso)
+	}
+}
+
+
+
 
 int main() {
 	twi_init();
@@ -38,6 +136,34 @@ int main() {
 
 	DDRD &= ~(1 << BUTTON_BUZZER); // Configura PD2 como entrada
 	PORTD |= (1 << BUTTON_BUZZER); // Habilita pull-up en PD2
+	uint16_t ubrr = F_CPU/16/9600 - 1;
+
+	_delay_ms(50);
+
+	twi_init();
+	twi_lcd_init();
+	twi_lcd_cmd(0x80);
+	
+	twi_lcd_clear();
+	char buffer[16];
+	
+		read_DHT_data(); // Leer datos de humedad y temperatura del DHT11
+		
+		// Mostrar humedad en la primera línea
+		twi_lcd_cmd(0x80);    // Ir a la primera línea
+		twi_lcd_msg("Humedad: ");
+		int_to_str(humidity_int, buffer); // Convertir humedad a cadena
+		twi_lcd_msg(buffer);
+		twi_lcd_msg("%");     // Añadir el símbolo de porcentaje
+		
+		// Mostrar temperatura en la segunda línea
+		twi_lcd_cmd(0xC0);    // Ir a la segunda línea
+		twi_lcd_msg("Temp: ");
+		int_to_str(temperature_int, buffer); // Convertir temperatura a cadena
+		twi_lcd_msg(buffer);
+		twi_lcd_msg("C");     // Añadir el símbolo de grados Celsius
+		
+		_delay_ms(2000); // Espera antes de actualizar de nuevo
 
 	while (1) {
 		// Control del LED
@@ -50,7 +176,7 @@ int main() {
 
 				twi_lcd_cmd(0xC0); // Cambiar a la segunda línea
 				twi_lcd_clear();
-				twi_lcd_msg(led_state ? "Prendio LED Roja " : "Apago LED Roja");
+				twi_lcd_msg(led_state ? "     Prendio BUZZER" : "       Apago BUZZER");
 			}
 		}
 
@@ -64,7 +190,7 @@ int main() {
 
 				twi_lcd_cmd(0xC0); // Cambiar a la segunda línea
 				twi_lcd_clear();
-				twi_lcd_msg(motor_state ? "Motor prendido" : "Motor apagado!");
+				twi_lcd_msg(motor_state ? "     Motor prendido" : "     Motor apagado!");
 			}
 		}
 
@@ -78,7 +204,7 @@ int main() {
 
 				twi_lcd_cmd(0xC0); // Cambiar a la segunda línea
 				twi_lcd_clear();
-				twi_lcd_msg(buzzer_state ? "Prendio Buzzer " : "Apago Buzzer");
+				twi_lcd_msg(buzzer_state ? "     Prendio LED" : "       Apago LED");
 			}
 		}
 	}
@@ -102,3 +228,6 @@ uint8_t SPI_MasterReceive() {
 	while (!(SPSR & (1 << SPIF))); // Espera a que se complete la recepción
 	return SPDR; // Devuelve el dato recibido
 }
+
+
+lcd_spi_maestro
